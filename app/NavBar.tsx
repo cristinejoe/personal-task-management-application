6009ff0f31f57c6747271014734beebe93626536
Cristine <cristinemartins01@gmail.com>
2024-02-05 12:37:40 -0600

    Build the navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,23 @@
+import { FaTasks } from "react-icons/fa"
+import Link from 'next/link'
+import React from 'react'
+
+const NavBar = () => {
+    const links = [
+        { label: "Dashboard", href: "/" },
+        { label: "Tasks", href: "/tasks/list" },
+    ];
+    return (
+        <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+            <Link href="/"><FaTasks /></Link>
+            <ul className='flex space-x-6'>
+                {links.map(link => 
+                    <Link key={link.href} 
+                    className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+                    href={link.href}>{link.label}</Link>)}
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
