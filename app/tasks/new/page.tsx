bae83cc9a19d6d720d81123e90f3d01ea45a7b3d
Cristine <cristinemartins01@gmail.com>
2024-02-07 10:53:44 -0600

    Extract the ErrorMessage component

--- app/tasks/new/page.tsx
+++ app/tasks/new/page.tsx
@@ -13,6 +13,7 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createTaskSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type taskForm = z.infer<typeof createTaskSchema>;
 
@@ -43,13 +44,13 @@ const newTaskPage = () => {
           <TextField.Root>
               <TextField.Input placeholder='Title' {...register('title')} />
           </TextField.Root>
-          {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
+          <ErrorMessage>{errors.title?.message}</ErrorMessage>
           <Controller
             name="description"
             control={control}
             render={({field}) => <SimpleMDE placeholder='Description' {...field} />}
           />
-          {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
+          <ErrorMessage>{errors.description?.message}</ErrorMessage>
           <Controller
             name="dueDate"
             control={control}
@@ -61,7 +62,7 @@ const newTaskPage = () => {
               />
             )}
           />
-          {errors.dueDate && <Text color='red' as='p'>{errors.dueDate.message}</Text>}
+          <ErrorMessage>{errors.dueDate?.message}</ErrorMessage>
           <Button>Create New Task</Button>
       </form>
     </div>
