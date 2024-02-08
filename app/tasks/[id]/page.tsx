7273920284a23002445bc31addc17905eae8c8ff
Cristine <cristinemartins01@gmail.com>
2024-02-08 11:30:38 -0600

    Add the edit button

--- app/tasks/[id]/page.tsx
+++ app/tasks/[id]/page.tsx
@@ -1,9 +1,11 @@
 import TaskStatusBadge from '@/app/components/TaskStatusBadge';
 import prisma from '@/prisma/client';
-import { Heading, Flex, Card, Text } from '@radix-ui/themes';
+import { Heading, Flex, Card, Text, Grid, Box, Button } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import React from 'react'
 import ReactMarkdown from 'react-markdown';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import Link from 'next/link';
 
 interface Props {
     params: { id: string }
@@ -20,16 +22,24 @@ const task = await prisma.task.findUnique({
   notFound();
     
   return (
-    <div>
-      <Heading>{task.title}</Heading>
-      <Flex className="space-x-3" my="2">
-        <TaskStatusBadge status={task.status} />
-        <Text>{task.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className='prose' mt='4'>
-        <ReactMarkdown>{task.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{initial: "1", md: "2"}} gap="5">
+      <Box>
+        <Heading>{task.title}</Heading>
+        <Flex className="space-x-3" my="2">
+          <TaskStatusBadge status={task.status} />
+          <Text>{task.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className='prose' mt='4'>
+          <ReactMarkdown>{task.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+      <Button>
+        <Pencil2Icon />
+        <Link href={`/tasks/${task.id}/edit`}>Edit Issue</Link>
+      </Button>
+      </Box>
+    </Grid>
   )
 }
 
