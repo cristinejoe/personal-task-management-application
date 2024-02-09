ccd3fa30784fe6a551bded08c7d42f660871b4a2
Cristine <cristinemartins01@gmail.com>
2024-02-09 13:21:37 -0600

    Delete a task

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import { Container, Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import NavBar from './NavBar';
-import './globals.css';
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Theme } from '@radix-ui/themes';
+import NavBar from './NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,9 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Theme>
           <NavBar />
-          <main className='p-5'>
-            <Container>{children}</Container>
-          </main>
+          <main className='p-5'>{children}</main>
         </Theme>
       </body>
     </html>
