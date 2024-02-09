7b08d246705e753b1685e429f0604791036bd81b
Cristine <cristinemartins01@gmail.com>
2024-02-09 17:34:46 -0600

    Add a drop-down menu to show the current user

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,11 @@
 "use client";
 
+import { Avatar, Box, DropdownMenu, Flex, Text } from '@radix-ui/themes';
+import classnames from 'classnames';
 import Link from 'next/link';
 import { usePathname } from "next/navigation";
-import React from 'react';
 import { FaTasks } from "react-icons/fa";
-import classnames from 'classnames';
-import { useSession } from 'next-auth/react';
-import { Box, Flex } from '@radix-ui/themes';
+import { useSession } from "next-auth/react";
 
 
 const NavBar = () => {
@@ -19,38 +18,61 @@ const NavBar = () => {
         { label: "Tasks", href: "/tasks/list" },
     ];
     return (
-        <nav className='border-b mb-5 px-5 py-3'>
-            <Flex justify='between'>
-                <Flex align='center' gap='3'>
-                    <Link href="/"><FaTasks /></Link>
-                    <ul className="flex space-x-6">
-                    {links.map((link) => (
-                        <li key={link.href}>
-                            <Link
-                            className={classnames({
-                                "text-zinc-900": link.href === currentPath,
-                                "text-zinc-500": link.href !== currentPath,
-                                "hover:text-zinc-800 transition-colors": true,
-                            })}
-                            href={link.href}
-                            >
-                            {link.label}
-                            </Link>
-                        </li>
-                    ))}
-                    </ul>
-                </Flex>
-                <Box>
-                {status === "authenticated" && (
-                <Link href="/api/auth/signout">Log out</Link>
-                )}
-                {status === "unauthenticated" && (
-                <Link href="/api/auth/signin">Login</Link>
-                )}
-            </Box>
-            </Flex>   
+        <nav className="border-b mb-5 px-5 py-3">
+            <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <FaTasks />
+            </Link>
+            <ul className="flex space-x-6">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={classnames({
+                      "text-zinc-900": link.href === currentPath,
+                      "text-zinc-500": link.href !== currentPath,
+                      "hover:text-zinc-800 transition-colors":
+                        true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback="?"
+                    size="2"
+                    radius="full"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">
+                      {session.user!.email}
+                    </Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                     <Link href="/api/auth/signout">Log out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
+        </Flex>
         </nav>
-    );
-};
+    )
+}
 
 export default NavBar
