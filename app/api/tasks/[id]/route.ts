71c227dc8ee358b96a3bf53f42e8b8143cebb3c6
Cristine <cristinemartins01@gmail.com>
2024-02-09 13:13:32 -0600

    Build an API for deleting tasks

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -27,3 +27,23 @@ export async function PATCH(
 
   return NextResponse.json(updatedTask);
 }
+
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string }}) {
+
+    const task = await prisma.task.findUnique({
+      where: { id: parseInt(params.id) }
+    });
+    if (!task) 
+      return NextResponse.json({ error: 'Invalid task' }, { status: 404 });  
+
+    await prisma.task.delete({
+      where: { id: task.id },
+      
+    });
+  
+    return NextResponse.json({});
+
+}
