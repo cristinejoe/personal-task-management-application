cd2c0a90cdcf6b4ed39a732ef6d99f3606346fcc
Cristine <cristinemartins01@gmail.com>
2024-02-09 13:05:59 -0600

    Show a confirmation dialog box

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
+import { Container, Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Theme } from '@radix-ui/themes';
-import NavBar from './NavBar'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import NavBar from './NavBar';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,7 +22,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <Theme>
           <NavBar />
-          <main className='p-5'>{children}</main>
+          <main className='p-5'>
+            <Container>{children}</Container>
+          </main>
         </Theme>
       </body>
     </html>
--- app/tasks/[id]/DeleteTaskButton.tsx
+++ app/tasks/[id]/DeleteTaskButton.tsx
@@ -1,9 +1,30 @@
-import { Button } from '@radix-ui/themes'
+'use client'
+
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 
 const DeleteTaskButton =  ({ taskId }: { taskId: number}) => {
     return (
-      <Button color="red">Delete Task</Button>
-    )
-  }
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Task</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this task? This action cannot be undone.
+        </AlertDialog.Description>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button variant='soft' color="gray">Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete Task</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+      </AlertDialog.Root>
+      
+    );
+  };
 
 export default DeleteTaskButton
