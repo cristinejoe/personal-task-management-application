73dc83a8344774a1e41c7518eb6b321b7574493d
Cristine <cristinemartins01@gmail.com>
2024-02-14 10:55:46 -0600

    Optimize performance using react cache

--- app/tasks/[id]/page.tsx
+++ app/tasks/[id]/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/prisma/client";
 import { Box, Flex, Grid } from "@radix-ui/themes";
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
+import { cache } from "react";
 import AssigneeSelect from "./AssigneeSelect";
 import DeleteTaskButton from "./DeleteTaskButton";
 import EditTaskButton from "./EditTaskButton";
@@ -12,12 +13,14 @@ interface Props {
   params: { id: string };
 }
 
+const fetchUser = cache((taskId: number) =>
+  prisma.task.findUnique({ where: { id: taskId } })
+);
+
 const TaskDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
 
-  const task = await prisma.task.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const task = await fetchUser(parseInt(params.id));
 
   if (!task) notFound();
 
@@ -40,9 +43,7 @@ const TaskDetailPage = async ({ params }: Props) => {
 };
 
 export async function generateMetadata({ params }: Props) {
-  const task = await prisma.task.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const task = await fetchUser(parseInt(params.id));
 
   return {
     title: task?.title,
